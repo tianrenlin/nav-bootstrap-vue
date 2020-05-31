b01d9bfd44765202f93bef18c99babc1df7ad991
一花一草一世界 <3287108795@qq.com>
2020-05-31 20:11:21 +0800

    使用requestAnimationFrame取代定时器实现首页时间滚动效果。解决时间框旋转滚动问题

--- src/components/time.vue
+++ src/components/time.vue
@@ -1,82 +1,79 @@
 <template>
-  <div>
-    <!-- 时钟部分 -->
-    <div class="time">
-      <!-- 时 -->
-      <ul class="t_ul">
-        <li class="front"></li>
-        <li class="top"></li>
-        <li class="bot"></li>
-        <li></li>
-        <li></li>
-        <li class="back"></li>
-      </ul>
-      <ul class="t_ul">
-        <li class="front"></li>
-        <li class="top"></li>
-        <li class="bot"></li>
-        <li></li>
-        <li></li>
-        <li class="back"></li>
-      </ul>
-      <!-- 分 -->
-      <ul class="t_ul">
-        <li class="front"></li>
-        <li class="top"></li>
-        <li class="bot"></li>
-        <li></li>
-        <li></li>
-        <li class="back"></li>
-      </ul>
-      <ul class="t_ul">
-        <li class="front"></li>
-        <li class="top"></li>
-        <li class="bot"></li>
-        <li></li>
-        <li></li>
-        <li class="back"></li>
-      </ul>
-      <!-- 秒 -->
-      <ul class="t_ul">
-        <li class="front"></li>
-        <li class="top"></li>
-        <li class="bot"></li>
-        <li></li>
-        <li></li>
-        <li class="back"></li>
-      </ul>
-      <ul class="t_ul">
-        <li class="front"></li>
-        <li class="top"></li>
-        <li class="bot"></li>
-        <li></li>
-        <li></li>
-        <li class="back"></li>
-      </ul>
-    </div>
-  </div>
+ <div class="time">
+    <!-- 时 -->
+    <ul class="box">
+          <li class="front"></li>
+          <li class="top"></li>
+          <li class="bottom"></li>
+          <li class="left"></li>
+          <li class="right"></li>
+          <li class="back"></li>
+        </ul>
+    <ul class="box">
+      <li class="front"></li>
+      <li class="top"></li>
+      <li class="bottom"></li>
+      <li class="left"></li>
+      <li class="right"></li>
+      <li class="back"></li>
+    </ul>
+    <!-- 分 -->
+    <ul class="box">
+      <li class="front"></li>
+      <li class="top"></li>
+      <li class="bottom"></li>
+      <li class="left"></li>
+      <li class="right"></li>
+      <li class="back"></li>
+    </ul>
+    <ul class="box">
+      <li class="front"></li>
+      <li class="top"></li>
+      <li class="bottom"></li>
+      <li class="left"></li>
+      <li class="right"></li>
+      <li class="back"></li>
+    </ul>
+    <!-- 秒 -->
+    <ul class="box">
+      <li class="front"></li>
+      <li class="top"></li>
+      <li class="bottom"></li>
+      <li class="left"></li>
+      <li class="right"></li>
+      <li class="back"></li>
+    </ul>
+    <ul class="box">
+      <li class="front"></li>
+      <li class="top"></li>
+      <li class="bottom"></li>
+      <li class="left"></li>
+      <li class="right"></li>
+      <li class="back"></li>
+    </ul>
+	</div>
 </template>
 
 <script>
   export default {
     methods: {
       showTime() {
-        var oul = document.querySelectorAll(".t_ul");
-        var back = document.querySelectorAll(".back");
-        var front = document.querySelectorAll(".front");
-        var Top = document.querySelectorAll(".top");
-        var bot = document.querySelectorAll(".bot");
-        var arr = timeArr();
-        
-        for (var i = 0; i < arr.length; i++) {
+        let oul = document.querySelectorAll(".box");
+        let back = document.querySelectorAll(".back");
+        let front = document.querySelectorAll(".front");
+        let Top = document.querySelectorAll(".top");
+        let bot = document.querySelectorAll(".bottom");
+        let arr = timeArr();
+        // 进行数字变更
+        for (let i = 0; i < arr.length; i++) {
           front[i].innerHTML = arr[i];
           back[i].innerHTML = arr[i] + 2 >= 10 ? (arr[i] + 2) % 10 : arr[i] + 2;
           Top[i].innerHTML = arr[i] + 3 >= 10 ? (arr[i] + 3) % 10 : arr[i] + 3;
           bot[i].innerHTML = arr[i] + 1 >= 10 ? (arr[i] + 1) % 10 : arr[i] + 1;
         }
-
+        // 获取时分秒
         function timeArr(){
-          var arr=[]
+          let arr=[]
           arr[0] = Math.floor(new Date().getHours() / 10);
           arr[1] = new Date().getHours() % 10;
           arr[2] = Math.floor(new Date().getMinutes() / 10);
@@ -85,48 +82,74 @@
           arr[5] = new Date().getSeconds() % 10;
           return arr
         }
-        // time()
+        // 1、===================进行动画-requestAnimationFrame==================
+        time()
         function time() {
-          var arr2 = timeArr();
-
-          for (var i = 0; i < oul.length; i++) {
-            if (front[i].innerHTML != arr2[i]) {
-              oul[i].classList.add("transition");
+          let arr2 = timeArr()
+          let prevArr=prevTime()
+          for (let i = 0; i < oul.length; i++) {
+            if (front[i].innerHTML != prevArr[i]) {
+              front[i].innerHTML = prevArr[i]
+              back[i].innerHTML = arr2[i] + 2 < 10 ? arr2[i] + 2 : (arr2[i] +2) % 10;
+              Top[i].innerHTML = arr2[i] + 3 < 10 ? arr2[i] + 3 : (arr2[i] +3) % 10;
+              bot[i].innerHTML = arr2[i] 
+              animation(oul[i])
             }
-            oul[i].index = i;
-            oul[i].addEventListener("webkitTransitionEnd", function () {
-
-              front[this.index].innerHTML = arr2[this.index];
-              back[this.index].innerHTML = arr2[this.index] + 2 < 10 ? arr2[this.index] + 2 : (arr2[this.index] +
-                2) % 10;
-              Top[this.index].innerHTML = arr2[this.index] + 3 < 10 ? arr2[this.index] + 3 : (arr2[this.index] +
-                3) % 10;
-              bot[this.index].innerHTML = arr2[this.index] + 1 < 10 ? arr2[this.index] + 1 : (arr2[this.index] +
-                1) % 10;
-              this.classList.remove("transition");
-
-            })
-
           }
-          // window.requestAnimationFrame(time);
+          // 执行动画，并请求浏览器在下一次重绘之前调用指定的函数来更新动画。根据屏幕刷新率(60Hz)来执行，放到后台后不执行。
+          window.requestAnimationFrame(time);
         }
+        // 设置动画
+        function animation(node){
+          node.animate([
+              {transform:'rotateY(-7deg) rotateX(0deg)'},
+              {transform:'rotateY(-7deg) rotateX(95deg)'}
+            ],{
+              duration:500,
+              fill:'forwards',
+              endDelay:500
+            })
+        }
+        // 处理旋转时上一个数字，同时处理因旋转产生视觉多出一秒问题
+        function prevTime(){
+          let arr=[]
+          timeArr().forEach((item,i)=>{
+            if(item-1<0){
+              switch(i){
+                case 0:{  // 时：十位
+                  item=2
+                };break;
+                case 2:{  // 分：十位
+                  item=5
+                };break;
+                case 4:{  // 秒：十位
+                  item=5
+                };break;
+                default: item=9;  // 其它个位
+              }
+            }else{
+              item-=1
+            }
+            arr.push(item)
+          })
+          return arr
+        }
+        // 2、===================进行动画-使用setInterval===================
         // setInterval(time, 1000)
       /*
-       "在开发环境下，很少使用间歇调用（setInterval），原因是后一个间歇调用很可能在前一个间歇调用结束前启动",
+      "在开发环境下，很少使用间歇调用（setInterval），原因是后一个间歇调用很可能在前一个间歇调用结束前启动,
       （如，回调当中的代码执行for循环时间比，比设定时间长，在回调还没执行完的时候，第二次定时任务就已经放到事件队列中去了，
       所以多用seTimeout代替setinterval）      
       */
-        
-        timeOut()
+      // 3、===================进行动画-使用setTimeout===================
+        // timeOut()
         function timeOut(){
-          clearTimeout(timer)
           let t=1000
-          let timer=null
           time()
-          timer=setTimeout(timeOut, t);
+          setTimeout(timeOut, t);
         }
 
-      }
+  }
     },
     mounted() {
       // 异步操作，保证页面渲染完成
@@ -138,92 +161,72 @@
 </script>
 
 <style lang="less" scoped>
-  .t_ul {
-    float: left;
-    padding-left: 0;
-    margin-right: 13px;
-    width: 35px;
-    height: 35px;
-    position: relative;
-    transform-style: preserve-3d;
-    transform: rotateX(5deg) rotateY(-7deg);
-    &:nth-child(2n){ // 1开始 
-      margin-left: 5px;
-      margin-right: 10px;
-    }
-    &:nth-child(2n-1){
-      margin-right: 0!important;
-    }
-    &:last-child{
-      margin-right: 0!important;
-    }
-  }
-
-
-  .transition {
-    transition: all 0.5s;
-    transform: rotateY(-7deg) rotateX(95deg);
-  }
-
   .time {
-    max-width: 345px;
-    width: 100%;
-    height: 60px;
-    margin: 40px auto;
-    display: flex;
-    justify-content: center;
-  }
-
-  .time .t_ul li {
-    text-align: center;
-    line-height: 30px;
-    font-size: 30px;
-    color: rgba(255, 255, 255, 0.2);
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    transform: translateZ(17px);
-    border: 1px solid #007ACC;
-    font-weight: 400;
-    text-shadow: 0 0 5px #29a1f1, 0 0 6px #29a1f1, 0 0 7px #29a1f1, 0 0 8px #29a1f1, 0 0 9px #29a1f1;
-    padding-left: 5px;
-    box-sizing: border-box;
-  }
-
-  .time li:nth-of-type(1) {
-    font-size: 30px;
-    text-shadow: 0 0 5px #fff, 0 0 20px #fefcc9;
-    line-height: 35px;
-    color: #007ACC;
-    transform: translateZ(17px);
-  }
-
-  .time li:nth-of-type(2) {
-    transform: rotateX(90deg) translateZ(17px)
-  }
-
-  .time li:nth-of-type(3) {
-    transform: rotateX(90deg) translateZ(-17px)
-  }
-
-  .time li:nth-of-type(4) {
-    transform: rotateY(90deg) translateZ(17px)
-  }
-
-  .time li:nth-of-type(5) {
-    transform: rotateY(90deg) translateZ(-17px)
-  }
-
-  .time li:nth-of-type(6) {
-    transform: translateZ(-17px);
-  }
-
-  /*@keyframes sk {
-    0 {
-        transform: rotateX(-10deg) rotateY(-15deg) rotateZ(-5deg)
+      max-width: 345px;
+      width: 100%;
+      height: 60px;
+      margin: 40px auto;
+      display: flex;
+      justify-content: center;
     }
-    100% {
-        transform: rotateX(10deg) rotateY(15deg) rotateZ(5deg)
+		.box{
+			float: left;
+		    padding-left: 0;
+		    margin-right: 13px;
+		    width: 35px;
+		    height: 35px;
+		    position: relative;
+		    transform-style: preserve-3d;
+		    transform: rotateX(5deg) rotateY(-7deg);
+		}
+		.box li{
+		    text-align: center;
+		    line-height: 35px;
+		    font-size: 30px;
+		    color: rgba(255, 255, 255, 0.2);
+		    width: 100%;
+		    height: 100%;
+		    position: absolute;
+		    transform: translateZ(17px);
+		    border: 1px solid #007ACC;
+		    font-weight: 400;
+		    text-shadow: 0 0 5px #29a1f1, 0 0 6px #29a1f1, 0 0 7px #29a1f1, 0 0 8px #29a1f1, 0 0 9px #29a1f1;
+		    
+		    box-sizing: border-box;
+		}
+
+	  .box .front {
+	    font-size: 30px;
+	    text-shadow: 0 0 5px #fff, 0 0 20px #fefcc9;
+	    line-height: 35px;
+	    color: #007ACC;
+	    /* padding-left: 5px; */
+	    transform: translateZ(17px);
+	  }
+
+	  .box .top {
+	    transform: rotateX(90deg) translateZ(17px)
+	  }
+
+	  .box .bottom{
+	  	color: #007ACC;
+	    transform: rotateX(270deg) translateZ(17px)
+	  }
+
+	  .box .left{
+	    transform: rotateY(90deg) translateZ(17px)
+	  }
+
+	  .box .right{
+	    transform: rotateY(90deg) translateZ(-17px)
+	  }
+
+	  .box .back{
+      color: rgba(0,0,0,0.35);
+	    transform: translateZ(-17px);
+	  }
+    .transition {
+      transition: all 0.5s;
+      transform: rotateY(-7deg) rotateX(95deg);
     }
-}*/
 </style>
